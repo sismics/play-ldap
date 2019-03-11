668ac582c467b33b24b45516208873209a6e855e
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2019-03-11 15:40:10 +0100

    Add LDAP connection wrapper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 ## Change Log
+### Version 1.2.0 (2019-03-11)
+- Add LDAP connection wrapper
 ### Version 1.1.0 (2019-03-01)
 - Add timeout configuration
 ### Version 1.0.0 (2019-02-28)
--- app/helpers/api/ldap/LdapUtil.java
+++ app/helpers/api/ldap/LdapUtil.java
@@ -1,10 +1,9 @@
 package helpers.api.ldap;
 
+import controllers.ldap.Ldap;
 import org.apache.commons.pool.impl.GenericObjectPool;
-import org.apache.directory.ldap.client.api.DefaultLdapConnectionFactory;
-import org.apache.directory.ldap.client.api.LdapConnectionConfig;
-import org.apache.directory.ldap.client.api.LdapConnectionPool;
-import org.apache.directory.ldap.client.api.ValidatingPoolableLdapConnectionFactory;
+import org.apache.directory.ldap.client.api.*;
+import play.Logger;
 import play.Play;
 
 /**
@@ -28,7 +27,9 @@ public class LdapUtil {
         GenericObjectPool.Config poolConfig = new GenericObjectPool.Config();
         poolConfig.whenExhaustedAction = GenericObjectPool.WHEN_EXHAUSTED_GROW;
         poolConfig.maxWait = 500;
-        return new LdapConnectionPool(new ValidatingPoolableLdapConnectionFactory(factory), poolConfig);
+        LdapConnectionPool pool = new LdapConnectionPool(new ValidatingPoolableLdapConnectionFactory(factory), poolConfig);
+        Logger.info("Connected to LDAP at " + config.getLdapHost() + ":" + config.getLdapPort());
+        return pool;
     }
 
     public static boolean isMock() {
@@ -40,7 +41,7 @@ public class LdapUtil {
     }
 
     private static Integer getLdapPort() {
-        return Integer.valueOf(Play.configuration.getProperty("ldap.port"));
+        return Integer.valueOf(Play.configuration.getProperty("ldap.port", "389"));
     }
 
     private static String getLdapAdminDn() {
@@ -57,5 +58,17 @@ public class LdapUtil {
 
     public static String getLdapBaseDn() {
         return Play.configuration.getProperty("ldap.baseDn");
+    }
+
+    public static void withLdapConnection(Runnable r) {
+        LdapConnection ldapConnection = null;
+        try {
+            Ldap.checkLdapConnection();
+            r.run();
+        } catch (Exception e) {
+            Logger.error(e, "Exception in LDAP runner");
+        } finally {
+            Ldap.withLdapFinally();
+        }
     }
 }
